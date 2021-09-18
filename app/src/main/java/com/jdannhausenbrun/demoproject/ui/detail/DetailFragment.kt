268db97c50eec555142c2a9ddcef21c6f27002be
Jacob Dannhausen-Brun <j.dannhausenbrun@climate.com>
2021-09-18 11:19:25 -0700

    First iteration

---
+++ app/src/main/java/com/jdannhausenbrun/demoproject/ui/detail/DetailFragment.kt
@@ -0,0 +1,44 @@
+package com.jdannhausenbrun.demoproject.ui.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.jdannhausenbrun.demoproject.databinding.FragmentDashboardBinding
+
+class DetailFragment : Fragment() {
+
+    private lateinit var detailViewModel: DetailViewModel
+    private var _binding: FragmentDashboardBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        detailViewModel =
+            ViewModelProvider(this).get(DetailViewModel::class.java)
+
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textDashboard
+        detailViewModel.text.observe(viewLifecycleOwner, Observer {
+            textView.text = it
+        })
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
